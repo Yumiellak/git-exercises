04fecec58fc01c557c808363a3edeb454d9815e7
ForceNight <89636568+ForceNight12@users.noreply.github.com>
2024-11-18 20:16:38 -0800

    Create SortArray.java

---
+++ SortArray.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void sortIntegers(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static void sortStrings(String[] arr) {
+        Arrays.sort(arr);
+    }
+}
